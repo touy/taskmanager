25b61288ad3f1890eb04e250fbad3173e45c292f
keophilavanh <nuengmamipoko@gmail.com>
2019-07-11 09:15:27 +0700

    commit

--- v2/src/app/pages/plan-document/add-documet/add-documet.component.ts
+++ v2/src/app/pages/plan-document/add-documet/add-documet.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router,ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
+import pouchdb from 'pouchdb';
 import { NbDialogService } from '@nebular/theme';
 import { ModleMenberComponent } from '../../project-jobs/modle-menber/modle-menber.component';
 import { ModleJobsComponent } from '../../project-jobs/modle-jobs/modle-jobs.component';
+import { Idocument,Odocument,Igijuser, MyDataBaseNames } from '../../../interface';
 
 @Component({
   selector: 'ngx-add-documet',
@@ -11,9 +14,89 @@ import { ModleJobsComponent } from '../../project-jobs/modle-jobs/modle-jobs.com
 })
 export class AddDocumetComponent implements OnInit {
 
-  constructor(private dialogService: NbDialogService,private router: Router) { }
+  private db: PouchDB.Database<{}>;
+  private Usertable: PouchDB.Database<{}>;
+  remoteCouch = 'http://admin:admin@localhost:5984/job-';
+
+
+  public strFrom ="";
+  public params={};
+  public js:any;
+  Doc: Idocument;
+  _selectedDoc: Idocument;
+
+ 
+  constructor(private dialogService: NbDialogService,private router: Router,public _Location:Location,private route: ActivatedRoute) {
+    this.Doc=new Odocument();
+ 
+    this.db = new pouchdb(MyDataBaseNames.dbdoc);
+  
+   }
 
   ngOnInit() {
+  }
+
+  getData(){
+    this.js = JSON.parse(localStorage.getItem('js'));
+    this.strFrom=this.js.from;
+  }
+  getParams()
+  {
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // {order: "popular"}
+        this.params=params;
+      });
+  }
+
+ 
+  
+
+  save(){
+      console.log(this.Doc);
+
+  
+      try{
+
+        this.Doc._id=(Math.random() * 1000000)+'';
+        console.log('add new');
+       
+        this.insert();
+       
+       
+        
+      }
+      catch(e){
+
+      }
+
+   
+    
+  }
+
+
+  insert(){
+
+  
+    this.db.put(this.Doc, { force: true }, (err, res) => {
+      if (err) {
+        console.log('err after put'
+        );
+        console.log(err);
+      } else {
+        console.log('after put');
+        console.log(res);
+      }
+    });
+  }
+
+
+  deletejob(){
+    this.db.remove(this._selectedDoc).then(res=>{
+
+    }).catch(err=>{
+      
+    })
   }
 
   goblack(){
--- v2/src/app/pages/project-jobs/modle-menber/modle-menber.component.ts
+++ v2/src/app/pages/project-jobs/modle-menber/modle-menber.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
 import { Router } from '@angular/router';
 import {Location} from '@angular/common'
+import pouchdb from 'pouchdb';
 import {Igijuser,Ogijuser,nano_time, MyDataBaseNames} from '../../../interface';
 
 @Component({
@@ -11,10 +12,32 @@ import {Igijuser,Ogijuser,nano_time, MyDataBaseNames} from '../../../interface';
   styleUrls: ['./modle-menber.component.scss']
 })
 export class ModleMenberComponent  {
+  private Usertable: PouchDB.Database<{}>;
 
-  constructor(protected ref: NbDialogRef<ModleMenberComponent> ,public _Location:Location,public router:Router) { }
+  userList: Igijuser[];
+  constructor(protected ref: NbDialogRef<ModleMenberComponent> ,public _Location:Location,public router:Router) {
+
+    this.Usertable = new pouchdb(MyDataBaseNames.dbuser);
+   }
 
   ngOnInit() {
+    this.loadUserList()
+  }
+
+  loadUserList() {
+    const pageSize = 10;
+    const offSet = 0;
+    const parent = this;
+    this.userList.length = 0;
+    this.userList = new Array<Igijuser>();
+    this.Usertable.allDocs({ limit: pageSize, skip: offSet, descending: true, include_docs: true }).then(res => {
+      console.log(res);
+      for (let index = 0; index < res.rows.length; index++) {
+        parent.userList.push(<Igijuser><unknown>res.rows[index].doc);
+      }
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
   
